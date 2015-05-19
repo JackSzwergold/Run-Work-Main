4824925b15d72205e2521e1c9302973aabe56ea2
Jack Szwergold <JackSzwergold@gmail.com>
2015-05-19 11:05:13 -0400

    First real commit.

---
+++ javascript/runwork_popup.js
@@ -0,0 +1,15 @@
+<!-- Begin
+function NewWindow(mypage, myname, w, h, scroll) {
+var winl = (screen.width - w) / 2;
+var wint = (screen.height - h) / 2;
+winprops = 'height='+h+',width='+w+',top='+wint+',left='+winl+',toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0'
+win = window.open(mypage, myname, winprops)
+if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
+}
+//  End -->
+
+<!-- Begin
+function royal_pop(URL,pop_feature,pop_width,pop_height) {
+eval("window.open(URL,'"+pop_feature+"','toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=0,left=10,top=10,"+pop_width+","+pop_height+"');");
+}
+// End -->
